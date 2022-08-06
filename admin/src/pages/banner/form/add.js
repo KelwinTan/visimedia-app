d2cd2ba5dd08e2ffb876467c176fc939dbd0ea07
gobiel <jonathangobiel13@gmail.com>
2022-08-06 23:53:35 +0700

    feat: add banner page

---
+++ admin/src/pages/banner/form/add.js
@@ -0,0 +1,66 @@
+import { Button, Form, Input } from "antd";
+import { useEffect, useState } from "react";
+import useBanner from "../../../hooks/api/useBanner";
+
+export default function BannerForm({ id }) {
+  const { create, update, getDetail } = useBanner();
+  const [detail, setDetail] = useState({});
+  const [image, setImage] = useState(null);
+
+  const onAdd = async ({ name }) => {
+    await create({ name, image });
+    setImage(null);
+  };
+
+  const onUpdate = async ({ name }) => {
+    await update({ name, image });
+    setImage(null);
+  };
+
+  const onFinish = ({ name }) => {
+    if (id) {
+      onUpdate({ name });
+    } else {
+      onAdd({ name });
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getDetail(id).then((data) => {
+        console.log({ data });
+      });
+    }
+  }, [id]);
+
+  return (
+    <Form
+      name="basic"
+      initialValues={detail}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: "Please input name" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Image"
+        name="image"
+        rules={[{ required: true, message: "Please input image" }]}
+      >
+        <Input type={"file"} onChange={(e) => setImage(e.target.files[0])} />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
--- admin/src/pages/banner/index.js
+++ admin/src/pages/banner/index.js
@@ -1,5 +1,30 @@
+import { Modal } from "antd";
+import { useCallback, useState } from "react";
+import BannerForm from "./form/add";
+import BannerTable from "./table";
+
 function Banner() {
-  return <div>banner</div>;
+  const [showModal, setShowModal] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
+
+  const onUpdate = useCallback((id) => {
+    setSelectedId(id);
+    showModal(true);
+  }, []);
+
+  return (
+    <>
+      <Modal
+        title="Banner Detail"
+        visible={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={null}
+      >
+        <BannerForm id={selectedId} />
+      </Modal>
+      <BannerTable onUpdate={onUpdate} />
+    </>
+  );
 }
 
 export default Banner;
