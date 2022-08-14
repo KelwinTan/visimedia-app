0823612ad069d11ebbd7b475b284ffe604a3ae69
Jonathan Gobiel <jonathangobiel13@gmail.com>
2022-08-15 00:11:39 +0700

    chore: revamp login design (2)

---
+++ admin/src/pages/auth/login/styles.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/css";
+
+export const styContainer = css`
+  width: 100%;
+  max-width: 350px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 3rem;
+  padding-left: 12px;
+  padding-right: 12px;
+`;
+
+export const styLogo = css`
+  max-width: 300px;
+  height: 100px;
+  object-fit: contain;
+`;
