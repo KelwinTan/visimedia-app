234d3a2977751fd70dae39a11ea19bf769b47822
Jonathan Gobiel <jonathangobiel13@gmail.com>
2022-08-15 00:11:39 +0700

    chore: revamp login design (2)

--- admin/src/assets/image.js
+++ admin/src/assets/image.js
@@ -1,3 +1,5 @@
 import VISIMEDIA_LOGO from "./visimedia-logo.webp";
+import VISIMEDIA_LOGO_DARK from "./visimedia-logo-dark.webp";
+import VISIMEDIA_LOGO_LIGHT from "./visimedia-logo-light.webp";
 
-export { VISIMEDIA_LOGO };
+export { VISIMEDIA_LOGO, VISIMEDIA_LOGO_DARK, VISIMEDIA_LOGO_LIGHT };
