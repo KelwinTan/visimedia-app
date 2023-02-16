5e5feaa2c14feaa31d33c127173b4b86345701b4
Jonathan Gobiel <jonathangobiel13@gmail.com>
2023-02-16 15:00:58 +0700

    chore: validate bayar must already verified

--- client/components/order/OrderDetail/index.jsx
+++ client/components/order/OrderDetail/index.jsx
@@ -60,13 +60,15 @@ export default function OrderDetail(props) {
             </>
           ))}
           <div className={cx(dFlex, css({ justifyContent: 'flex-end' }))}>
-            <Link href={`/payment/${props.data.order_details.payment_id}`}>
-              <a>
-                <Button primary classnames={cx(hover, css({ width: 200 }))}>
-                  Bayar
-                </Button>
-              </a>
-            </Link>
+            {Boolean(props.data.order_details.order_verified) && (
+              <Link href={`/payment/${props.data.order_details.payment_id}`}>
+                <a>
+                  <Button primary classnames={cx(hover, css({ width: 200 }))}>
+                    Bayar
+                  </Button>
+                </a>
+              </Link>
+            )}
           </div>
         </Card.Body>
       </Card>
